2a788154fd7495ed012e1304d8b477aa45336e7f
GaoXiong <xiong.gao@shopee.com>
2023-02-11 09:07:58 +0800

    chore

---
+++ src/types/IResData.ts
@@ -0,0 +1,5 @@
+export interface IResData<T> {
+  code: number;
+  data: T;
+  msg: string;
+}
---
+++ src/utils/common/auth.ts
@@ -0,0 +1,18 @@
+import type { IUserInfo } from '@/types/IUserInfo';
+import * as store from 'store';
+
+export const getToken = (): string => {
+  return store.get('token') || '';
+};
+
+export const setToken = (token: string) => {
+  store.set('token', token);
+};
+
+export const getUserInfo = (): IUserInfo | null => {
+  return store.get('userInfo') || null;
+};
+
+export const getUserId = () => {
+  return getUserInfo()?.id || null;
+};
